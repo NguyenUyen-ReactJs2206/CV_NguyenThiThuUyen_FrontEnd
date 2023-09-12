5b2668dd0259f0bd0a905adc6e3804651539db43
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-09-13 00:26:59 +0700

    completed sub content of ability to work

--- MyProfile/src/components/AbilityToWork/AbilityToWork.tsx
+++ MyProfile/src/components/AbilityToWork/AbilityToWork.tsx
@@ -1,20 +1,21 @@
+import { useTranslation } from 'react-i18next'
+
 export default function AbilityToWork() {
+  const { t } = useTranslation('abilitytowork')
   return (
-    <div id='ability-to-work' className='py-16 px-4 min-h-[100vh]'>
-      <div className='container mx-auto text-left'>
-        <h2 className='text-4xl mb-4 font-semibold py-4 text-center text-blue'>Ability to work</h2>
+    <div id='ability-to-work' className='py-16 min-h-[100vh]'>
+      <div className='container mx-auto px-10 text-left'>
+        <h2 className='text-4xl mb-4 font-semibold py-4 text-center text-blue'>{t('title')}</h2>
         <div className='grid grid-cols-12 gap-4'>
           <div className='lg:col-span-4 col-span-12'>
             <h3 className='text-lg font-bold'>HTML / CSS and Tailwind CSS / Bootstrap</h3>
           </div>
           <div className='lg:col-span-8 col-span-12'>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex suscipit dolor deserunt eius velit
-              quidem cupiditate voluptatem asperiores? Labore, deleniti. Est sint odit reiciendis expedita cum ex modi
-              doloribus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, quo quidem officia laboriosam,
-              aliquam adipisci delectus hic ipsum, eius tempora tenetur. Pariatur, sit. Impedit labore quae rem
-              voluptatem officiis delectus!
-            </p>
+            <ul className='list-disc'>
+              <li>{t('html&css1')}</li>
+              <li>{t('html&css2')}</li>
+              <li>{t('html&css3')}</li>
+            </ul>
           </div>
         </div>
         <hr className='my-4' />
@@ -23,13 +24,12 @@ export default function AbilityToWork() {
             <h3 className='text-lg font-bold'>Java Script / Type Script</h3>
           </div>
           <div className='lg:col-span-8 col-span-12'>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex suscipit dolor deserunt eius velit
-              quidem cupiditate voluptatem asperiores? Labore, deleniti. Est sint odit reiciendis expedita cum ex modi
-              doloribus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, quo quidem officia laboriosam,
-              aliquam adipisci delectus hic ipsum, eius tempora tenetur. Pariatur, sit. Impedit labore quae rem
-              voluptatem officiis delectus!
-            </p>
+            <ul className='list-disc'>
+              <li>{t('javascript&typescript1')}</li>
+              <li>{t('javascript&typescript2')}</li>
+              <li>{t('javascript&typescript3')}</li>
+              <li>{t('javascript&typescript4')}</li>
+            </ul>
           </div>
         </div>
         <hr className='my-4' />
@@ -38,28 +38,27 @@ export default function AbilityToWork() {
             <h3 className='text-lg font-bold'>Framework</h3>
           </div>
           <div className='lg:col-span-8 col-span-12'>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex suscipit dolor deserunt eius velit
-              quidem cupiditate voluptatem asperiores? Labore, deleniti. Est sint odit reiciendis expedita cum ex modi
-              doloribus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, quo quidem officia laboriosam,
-              aliquam adipisci delectus hic ipsum, eius tempora tenetur. Pariatur, sit. Impedit labore quae rem
-              voluptatem officiis delectus!
-            </p>
+            <ul className='list-disc'>
+              <li>{t('framework1')}</li>
+              <li>{t('framework2')}</li>
+              <li>{t('framework3')}</li>
+              <li>{t('framework4')}</li>
+            </ul>
           </div>
         </div>
         <hr className='my-4' />
         <div className='grid grid-cols-12 gap-4'>
           <div className='lg:col-span-4 col-span-12'>
-            <h3 className='text-lg font-bold'>The ability to work in groups</h3>
+            <h3 className='text-lg font-bold'>{t('workwithgroup')}</h3>
           </div>
           <div className='lg:col-span-8 col-span-12'>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex suscipit dolor deserunt eius velit
-              quidem cupiditate voluptatem asperiores? Labore, deleniti. Est sint odit reiciendis expedita cum ex modi
-              doloribus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, quo quidem officia laboriosam,
-              aliquam adipisci delectus hic ipsum, eius tempora tenetur. Pariatur, sit. Impedit labore quae rem
-              voluptatem officiis delectus!
-            </p>
+            <ul className='list-disc'>
+              <li>{t('workwithgroup1')}</li>
+              <li>{t('workwithgroup2')}</li>
+              <li>{t('workwithgroup3')}</li>
+              <li>{t('workwithgroup4')}</li>
+              <li>{t('workwithgroup5')}</li>
+            </ul>
           </div>
         </div>
       </div>
