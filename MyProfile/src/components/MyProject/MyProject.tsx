4c9552f8ef8a74cffecd7080149b9a8238a1fe50
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-09-16 00:00:19 +0700

    My project

--- MyProfile/src/components/MyProject/MyProject.tsx
+++ MyProfile/src/components/MyProject/MyProject.tsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom'
 import { projectDetailApi } from 'src/api/projectDetail.api'
+import IMAGE_HYGEE from 'src/assets/images/hygge-event-project.png'
+import IMAGE_SHOPEE from 'src/assets/images/shopee-project.png'
+import IMAGE_ARTICLE from 'src/assets/images/articles-project.png'
 
 export default function MyProject() {
   return (
@@ -8,9 +11,44 @@ export default function MyProject() {
         <div className='container mx-auto text-center'>
           <h2 className='text-4xl font-semibold py-4 text-blue'>My Project</h2>
           <div className='mt-10'>
-            <Link to={projectDetailApi.shopee.path}>Shoppe</Link>
+            <div className='grid grid-cols-12 gap-4'>
+              <div className='lg:col-span-4 sm:col-span-6 col-span-12'>
+                <Link to={projectDetailApi.shopee.path}>
+                  <div className='w-full lg:h-[370px] md:h-[320px] h-[300px]  bg-red-300'>
+                    <img
+                      src={`${IMAGE_SHOPEE}`}
+                      alt={`${IMAGE_SHOPEE}`}
+                      className='w-full h-full object-cover transition-all duration-500'
+                    />
+                  </div>
+                </Link>
+              </div>
+              <div className='lg:col-span-4 sm:col-span-6 col-span-12'>
+                <Link to={projectDetailApi.article.path}>
+                  <div className='w-full lg:h-[370px] md:h-[320px] h-[300px]  bg-red-300'>
+                    <img
+                      src={`${IMAGE_ARTICLE}`}
+                      alt={`${IMAGE_ARTICLE}`}
+                      className='w-full h-full object-cover transition-all duration-500'
+                    />
+                  </div>
+                </Link>
+              </div>
+              <div className='lg:col-span-4 sm:col-span-6 col-span-12'>
+                <Link to={projectDetailApi.event.path}>
+                  <div className='w-full lg:h-[370px] md:h-[320px] h-[300px]  bg-red-300'>
+                    <img
+                      src={`${IMAGE_HYGEE}`}
+                      alt={`${IMAGE_HYGEE}`}
+                      className='w-full h-full object-cover transition-all duration-500'
+                    />
+                  </div>
+                </Link>
+              </div>
+            </div>
+            {/* <Link to={projectDetailApi.shopee.path}>Shoppe</Link>
             <Link to={projectDetailApi.article.path}>Article</Link>
-            <Link to={projectDetailApi.event.path}>Event</Link>
+            <Link to={projectDetailApi.event.path}>Event</Link> */}
           </div>
         </div>
       </div>
